dcfa16f3835a78d5cd3c0eaf780268154739b1b1
Nihal Konda <nihal.konda@sjsu.edu>
2020-03-15 00:46:26 -0700

    Java CLI Commit

--- CabBuddies-Library/src/com/cabbuddieslib/utils/Basic.java
+++ CabBuddies-Library/src/com/cabbuddieslib/utils/Basic.java
@@ -9,25 +9,36 @@ import java.io.IOException;
 public class Basic {
 
 	public static void successPrint() {
-		File file = new File("success.txt"); 
-		  
-		  BufferedReader br = null;
-		try {
-			br = new BufferedReader(new FileReader(file));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-		  
-		  String st; 
-		  try {
-			while ((st = br.readLine()) != null) { 
-			    System.out.println(st); 
-			  }
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
+		
+		System.out.println(" ________  ___       ___               ___  ________  ________          ___       __   _______   ___       ___          \n" + 
+				"|\\   __  \\|\\  \\     |\\  \\             |\\  \\|\\_____  \\|\\_____  \\        |\\  \\     |\\  \\|\\  ___ \\ |\\  \\     |\\  \\         \n" + 
+				"\\ \\  \\|\\  \\ \\  \\    \\ \\  \\            \\ \\  \\\\|___/  /|\\|___/  /|       \\ \\  \\    \\ \\  \\ \\   __/|\\ \\  \\    \\ \\  \\        \n" + 
+				" \\ \\   __  \\ \\  \\    \\ \\  \\            \\ \\  \\   /  / /    /  / /        \\ \\  \\  __\\ \\  \\ \\  \\_|/_\\ \\  \\    \\ \\  \\       \n" + 
+				"  \\ \\  \\ \\  \\ \\  \\____\\ \\  \\____        \\ \\  \\ /  /_/__  /  /_/__        \\ \\  \\|\\__\\_\\  \\ \\  \\_|\\ \\ \\  \\____\\ \\  \\____  \n" + 
+				"   \\ \\__\\ \\__\\ \\_______\\ \\_______\\       \\ \\__\\\\________\\\\________\\       \\ \\____________\\ \\_______\\ \\_______\\ \\_______\\\n" + 
+				"    \\|__|\\|__|\\|_______|\\|_______|        \\|__|\\|_______|\\|_______|        \\|____________|\\|_______|\\|_______|\\|_______|\n" + 
+				"                                                                                                                        \n" + 
+				"                                                                                                                        \n" + 
+				"                                                                                                                        ");
+//		File file = new File("success.txt"); 
+//		  
+//		  BufferedReader br = null;
+//		try {
+//			br = new BufferedReader(new FileReader(file));
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} 
+//		  
+//		  String st; 
+//		  try {
+//			while ((st = br.readLine()) != null) { 
+//			    System.out.println(st); 
+//			  }
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} 
 	}
 	
 }
