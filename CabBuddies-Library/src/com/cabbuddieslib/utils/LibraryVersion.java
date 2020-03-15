bd4945e605c75e1cdbcfc890945d5157d9a2398d
Nihal Konda <nihal.konda@sjsu.edu>
2020-03-14 23:38:39 -0700

    Java CLI Commit

--- CabBuddies-Library/src/com/cabbuddieslib/utils/LibraryVersion.java
+++ CabBuddies-Library/src/com/cabbuddieslib/utils/LibraryVersion.java
@@ -2,6 +2,6 @@ package com.cabbuddieslib.utils;
 
 public class LibraryVersion {
 
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	
 }
---
+++ CabBuddies-Library/src/com/cabbuddieslib/utils/WhitelistIP.java
@@ -0,0 +1,10 @@
+package com.cabbuddieslib.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class WhitelistIP {
+
+	public static final ArrayList<String> jwtValidation = new ArrayList<String>(Arrays.asList("0:0:0:0:0:0:0:1"));
+	
+}
