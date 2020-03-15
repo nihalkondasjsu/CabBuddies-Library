a115d1e4be9fd2c09be006743e669247dc15fac5
Nihal Konda <nihal.konda@sjsu.edu>
2020-03-14 20:31:56 -0700

    Secretkey adjusted

--- CabBuddies-Library/src/com/cabbuddieslib/utils/AESCrypto.java
+++ CabBuddies-Library/src/com/cabbuddieslib/utils/AESCrypto.java
@@ -9,11 +9,10 @@ public class AESCrypto {
 	final static Base64.Encoder encorder = Base64.getEncoder();
     final static Base64.Decoder decorder = Base64.getDecoder();
     
-    private static final String MOST_SECURE_KEY = "0D434472240F828DFD9439D8AFC16EF894A57762DDCF10E0E253782833C7EE7A";
-    private static final String LEAST_SECURE_KEY = "F66BBF8179937F5070978432B3067CD699E8B495FED4EE35582750C678816850";
+    private static final String MOST_SECURE_KEY = "W)aN!t(z?GZ)BA83@.r}n2wZ-Vm`t[\\W";
+    private static final String LEAST_SECURE_KEY = ".m\"JL/?NSbMVzhKx4nje,>Cd8xvfn,4E";
     
     static private Cipher cipher(int opmode, String secretKey) throws Exception{
-    	secretKey = secretKey.substring(0, Math.min(31, secretKey.length()-1));
         if(secretKey.length() != 32) throw new RuntimeException("SecretKey length is not 32 chars");
         Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
         SecretKeySpec sk = new SecretKeySpec(secretKey.getBytes(), "AES");
